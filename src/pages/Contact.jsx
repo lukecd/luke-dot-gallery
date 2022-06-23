a7499217d15370edf8474a74d44efd640deb0ee8
The Luke <luke@SPStories.com>
2022-06-23 15:46:55 +0700

    added form validation

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -7,14 +7,14 @@ const Contact = () => {
         <form action='https://getform.io/f/19f5b563-72f2-49a9-a5a0-faa9d50c4b5e' method='post' className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                <p className='text-4xl font-bold inline border-b-4 border-[#f36c3d]'>contact ...</p>
-               <p className='py-4'>Working on something fun? use the form, or email me Luke AT SPstories.com.</p>
+               <p className='py-4'>Working on something fun? Use the form, or email me Luke AT SPstories.com.</p>
                <p className='py-4'>
                 <Social />
                </p>
             </div>
-            <input className='p-2 text-black' type="text" placeholder='name' name='name' />
-            <input className='my-4 p-2 text-black' type="email" placeholder='email' name='email' />
-            <textarea className='p-2 text-black' name='message' rows='10' placeholder='message'></textarea>
+            <input className='p-2 text-black' type="text" placeholder='name' name='name' required/>
+            <input className='my-4 p-2 text-black' type="email" placeholder='email' name='email' required/>
+            <textarea className='p-2 text-black' name='message' rows='10' placeholder='message' required></textarea>
             <button className='hover:bg-[#f36c3d] hover:border-[#f36c3d] border-2 px-4 py-3 my-8 mx-auto flex items-center'>let's collaborate</button>
         </form>
     </div>
